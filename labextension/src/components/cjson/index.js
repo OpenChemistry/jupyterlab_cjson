939bd074e7ba34d53da13b22e5800fcdb089cf09
Chris Harris <chris.harris@kitware.com>
2017-06-14 14:30:51 -0400

    Inject material-ui provider

--- labextension/src/components/cjson/index.js
+++ labextension/src/components/cjson/index.js
@@ -5,6 +5,14 @@ import './index.css';
 import StructureComponent from './structure'
 import VibrationalModesComponent from './vibrational'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+//Needed for onTouchTap
+//http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+
 export default class CJSONComponent extends React.Component {
 
   constructor(props) {
@@ -25,12 +33,16 @@ export default class CJSONComponent extends React.Component {
   render() {
     const { data, metadata } = this.props;
     return (
-      <div className='oc-cjson'>
-        { metadata.structure && <StructureComponent data={this.props.data} metadata={metadata} animateMode={this.state.animateMode}/>
-        }
-        {'vibrations' in this.props.data && metadata.vibrational &&
-          <VibrationalModesComponent data={this.props.data.vibrations} metadata={metadata} onBarClick={this.onBarClick}/>
-        }
+     <div>
+        <MuiThemeProvider>
+          <div className='oc-cjson'>
+            { metadata.structure && <StructureComponent data={this.props.data} metadata={metadata} animateMode={this.state.animateMode}/>
+            }
+            {'vibrations' in this.props.data && metadata.vibrational &&
+              <VibrationalModesComponent data={this.props.data.vibrations} metadata={metadata} onBarClick={this.onBarClick}/>
+            }
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
