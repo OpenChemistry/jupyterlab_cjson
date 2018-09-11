78b2ac29cc2dbd4a1584c9a619b0f30187fd873c
Chris Harris <chris.harris@kitware.com>
2018-09-11 15:27:52 -0400

    Remove unused type declarations

--- labextension/src/types/mongochemclient.d.ts
+++
@@ -1,8 +0,0 @@
-// import * as React from 'react'
-
-// declare module 'mongochemclient' {
-//   export class VibrationalModesChart extends React.Component<any, any> {}
-//   export class CalculationContainer extends React.Component<any, any> {}
-//   export class CalculationMonitorTableContainer extends React.Component<any, any> {}
-//   export var authenticate: (token: string, redirect:boolean) => void
-// }
